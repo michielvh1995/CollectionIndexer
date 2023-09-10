38bbb0cb63a235b89621593364485de49a8c6d5c
Michiel <michielvh1995@gmail.com>
2023-09-10 12:58:32 +0200

    Now made the input fields dynamic along with the possible finishes. See issue #13 for full explanation.

--- src/app/add-card/card-selector/components/card-selector.component.ts
+++ src/app/add-card/card-selector/components/card-selector.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Card, CardVersion } from '../../../shared/models/card';
 import { ScryfallAPIService } from '../../../shared/scryfallAPI/scyfall-api.service';
 
@@ -9,28 +9,55 @@ import { ScryfallAPIService } from '../../../shared/scryfallAPI/scyfall-api.serv
   styleUrls: ['../pages/card-selector.component.scss']
 })
 export class CardSelectorComponent {
-  constructor(private scryfallAPIService : ScryfallAPIService) {}
+  constructor(
+    private scryfallAPIService : ScryfallAPIService,
+    private fb: FormBuilder) {}
   
   image_url : string = "";
   promotypes? : string[];
   finishes? : string[];
 
+  tc = this.fb.array([]);
+
+  finishCounterForm = this.fb.group({
+    cardCounts: this.tc
+  });
+
+  // This function is used to dynamically generate the form controls; 
+  // we need one FromControl per type of finish. SOmetimes this is one, two, or more
+  setupFormControl() {    
+    if(this.finishes === undefined)
+      return;
+    
+      // Per finish we create one numeric input
+    for (let i = 0; i < this.finishes.length; i++)      
+      this.tc.push(this.fb.control(0));
+  }
+
   ngOnInit() {
     // Call the scryfall API for information
     this.getInformation();
+    
   }
 
   getInformation() {
     this.scryfallAPIService.getCardInformation(this.card.versions[0].set_code, this.card.versions[0].number).subscribe( res => {
       if(res.image_uris !== undefined)
         this.image_url = res.image_uris["normal"];
+        
 
         if(res.promo_types !== undefined)
-          this.promotypes = res.promo_types
+          this.promotypes = res.promo_types;
+          
+        
         if(res.finishes !== undefined)
-          this.finishes = res.finishes
+          this.finishes = res.finishes;
+
+        // Since we require the information from the scryfallAPI we call the setup form control here
+        this.setupFormControl();
     })
   }
+
 
 
   cardCounterForm = new FormGroup({
